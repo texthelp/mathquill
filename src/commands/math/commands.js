f54e80dbb513a65b332a3ebaf3e4d8a176362fa9
Ryan Graham <r.graham@texthelp.com>
2018-05-25 11:30:22 +0100

    Add basic `\cancel` command support

--- src/commands/math/commands.js
+++ src/commands/math/commands.js
@@ -83,6 +83,7 @@ LatexCmds.overrightarrow = bind(Style, '\\overrightarrow', 'span', 'class="mq-no
 LatexCmds.overleftarrow = bind(Style, '\\overleftarrow', 'span', 'class="mq-non-leaf mq-overarrow mq-arrow-left"');
 LatexCmds.overleftrightarrow = bind(Style, '\\overleftrightarrow', 'span', 'class="mq-non-leaf mq-overarrow mq-arrow-both"');
 LatexCmds.overarc = bind(Style, '\\overarc', 'span', 'class="mq-non-leaf mq-overarc"');
+LatexCmds.cancel = bind(Style, '\\cancel', 'span', 'class="mq-non-leaf mq-cancel"'); 
 LatexCmds.dot = P(MathCommand, function(_, super_) {
     _.init = function() {
         super_.init.call(this, '\\dot', '<span class="mq-non-leaf"><span class="mq-dot-recurring-inner">'
